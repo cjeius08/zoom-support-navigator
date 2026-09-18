1620eb0b2d728669d611fe1f8a9038e39e7e8017
cjeius08 <cjeius08@gmail.com>
2026-09-19 00:28:52 +0800

    Remove obsolete Navigator feedback ref import

--- src/features/navigator/Navigator.jsx
+++ src/features/navigator/Navigator.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { PROCESSES } from '../../data/processes'
 import { ProcessDrawer } from './ProcessDrawer'
 import { searchProcesses } from './smartSearch'
