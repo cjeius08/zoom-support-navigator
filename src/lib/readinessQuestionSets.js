0e5ea167f2a75858f59785e5f94dbc619d486168
cjeius08 <cjeius08@gmail.com>
2026-09-19 06:20:24 +0800

    Add Readiness Part 4 question-set identifier

--- src/lib/readinessQuestionSets.js
+++ src/lib/readinessQuestionSets.js
@@ -2,4 +2,5 @@ export const READINESS_QUESTION_SETS = {
   generalScenarios: 'zoom_general_scenarios_v1',
   deviceNavigation: 'zoom_device_navigation_v1',
   troubleshootingJudgment: 'zoom_troubleshooting_judgment_v1',
+  scopeReferralJudgment: 'zoom_scope_referral_judgment_v1',
 }
