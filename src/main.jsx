4c8abfd1f02c3f27935e1ffaa57548224ba6c735
cjeius08 <cjeius08@gmail.com>
2026-09-20 13:36:40 +0800

    Load horizontal shell revamp styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './ozzie-brand-polish.css'
+import './ozzie-brand-polish.css'\nimport './features/shell/headerNavRevamp.css'
 
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
