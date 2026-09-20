2940cdf64e5c55887b529792705577b91af3bd9a
cjeius08 <cjeius08@gmail.com>
2026-09-21 03:23:56 +0800

    Install new 103-avatar catalog

--- src/features/profile/avatarCatalog.js
+++ src/features/profile/avatarCatalog.js
@@ -1,4 +1,8 @@
-export const AVATAR_IDS = Object.freeze([])
+import { assetUrl } from '../../lib/assetUrl'
 
-export const isApprovedAvatarId = () => false
-export const avatarUrl = () => null
+export const AVATAR_IDS = Object.freeze(
+  Array.from({ length: 103 }, (_, index) => `avatar_${String(index + 1).padStart(3, '0')}`),
+)
+
+export const isApprovedAvatarId = avatarId => AVATAR_IDS.includes(avatarId)
+export const avatarUrl = avatarId => isApprovedAvatarId(avatarId) ? assetUrl(`avatars/${avatarId}.webp`) : null
