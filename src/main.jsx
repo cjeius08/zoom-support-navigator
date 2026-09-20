b29cb29c429194aff7992af5883d760c0aa9cd2a
cjeius08 <cjeius08@gmail.com>
2026-09-20 13:46:57 +0800

    Fix shell stylesheet import newline

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './ozzie-brand-polish.css'\nimport './features/shell/headerNavRevamp.css'
+import './ozzie-brand-polish.css'
+import './features/shell/headerNavRevamp.css'
 
 createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>)
