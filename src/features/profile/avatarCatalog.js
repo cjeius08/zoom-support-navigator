9b01732153a87945cbe1d849b53ed7e965b46c82
cjeius08 <cjeius08@gmail.com>
2026-09-21 03:00:28 +0800

    Clear avatar catalog

--- src/features/profile/avatarCatalog.js
+++ src/features/profile/avatarCatalog.js
@@ -1,12 +1,4 @@
-const EXCLUDED_AVATAR_IDS = new Set(
-  Array.from({ length: 27 }, (_, index) => `avatar_${String(index + 151).padStart(3, '0')}`),
-)
+export const AVATAR_IDS = Object.freeze([])
 
-export const AVATAR_IDS = Object.freeze(
-  Array.from({ length: 187 }, (_, index) => `avatar_${String(index + 1).padStart(3, '0')}`)
-    .filter(id => !EXCLUDED_AVATAR_IDS.has(id)),
-)
-
-export const isApprovedAvatarId = avatarId => AVATAR_IDS.includes(avatarId)
-export const avatarUrl = avatarId => isApprovedAvatarId(avatarId) ? assetUrl(`avatars/${avatarId}.png`) : null
-import { assetUrl } from '../../lib/assetUrl'
+export const isApprovedAvatarId = () => false
+export const avatarUrl = () => null
